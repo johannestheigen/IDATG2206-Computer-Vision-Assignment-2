fc25b5c0cc0a4aee7cd9291f95a244c5b8bdfe3d
Johannes Nupen Theigen <johannes.theigen@gmail.com>
2026-03-19 12:41:03 +0100

    Update image to process and add gray scale conversion

--- noise_addition_removal/noise_addition.m
+++ noise_addition_removal/noise_addition.m
@@ -4,20 +4,21 @@ clc
 clear all
 close all
 
-I = imread("cameraman.tif"); % Read the original image
+I = imread("images/Task1a_CV.bmp"); % Read the original image
+I_gray = rgb2gray(I); % Convert the image to gray scale
 
-I_salt = imnoise(I, "salt & pepper"); % Apply salt and pepper noise to the image
-I_gaussian = imnoise(I, "gaussian");  % Apply gaussian noise to the image
+I_salt_pepper = imnoise(I_gray, "salt & pepper"); % Apply salt and pepper noise to the image
+I_gaussian = imnoise(I_gray, "gaussian");  % Apply gaussian noise to the image
 
 % Plot the original image and its noisy versions
 figure(1);
 
 subplot(1, 3, 1);
-imshow(I);
-title('Original Image');
+imshow(I_gray);
+title('Original Image in gray scale');
 
 subplot(1, 3, 2);
-imshow(I_salt);
+imshow(I_salt_pepper);
 title('Image with salt & pepper noise');
 
 subplot(1, 3, 3);
