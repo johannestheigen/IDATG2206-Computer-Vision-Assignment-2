ea1a66a91251f5c1d98fd3a2c79919e4a7f3dff4
Johannes Nupen Theigen <johannes.theigen@gmail.com>
2026-03-20 14:45:47 +0100

    Change filter to arithmetic mean filter and add mean squared error (mse) calculation functionality

--- noise_addition_removal/noise_gaussian.m
+++ noise_addition_removal/noise_gaussian.m
@@ -1,28 +1,44 @@
 % MATLAB program that manually adds gaussian noise to an image
-% and removes it using a manually implemented median filter.
+% and removes it using a manually implemented median filter.It also 
+% calculates the mean squared error (mse) to determine the similarity
+% between the original image and the denoised version.
 clc
 clear all
 close all
 
-I = imread("images/Task1a_CV.bmp"); % Read the original image
+img = "images/Task1a_CV.bmp"; % The file path of the image to read
+I = imread(img); % Read the original image
+
 I_gray = double(rgb2gray(I)); % Convert the image from rgb grayscale      
 
-gaussian_noise = randn(size(I_gray)) * 15; % Generate Gaussian noise, standard deviation = 15 controls noise strength
+gaussian_noise = randn(size(I_gray)) * 25; % Generate Gaussian noise, standard deviation = 15 controls noise strength
 I_gaussian = I_gray + gaussian_noise;      % Add Gaussian noise to the grayscale image
 
-I_padded = padarray(I_gaussian, [1 1], 0, 'both'); % Zero pad by 1 to handle border pixels for 3x3 kernel
+I_padded = padarray(I_gaussian, [2 2], 'replicate', 'both'); % Pad the image to handle border pixels for 3x3 neighbourhood
 
 [i, j] = size(I_padded); % Get padded image dimensions for loop bounds
 
 I_denoised = zeros(size(I_gaussian)); % Initialise output image with same dimensions as input
 
-% Apply median filter to remove the noise
-for row = 2:(i-1)
-    for col = 2:(j-1)
-        sub = I_padded(row-1:row+1, col-1:col+1); % Extract 3x3 neighbourhood around current pixel
-        I_denoised(row-1, col-1) = median(sub(:)); % Store median value in output image 
+% Apply arithmetic mean filter to remove the noise
+for row = 3:(i-2)
+    for col = 3:(j-2)
+        sub = I_padded(row-2:row+2, col-2:col+2); % Extract 5x5 neighbourhood around current pixel
+        [m, n] = size(sub); % Retrieve the dimensions of the neighbourhood for mean calculation
+        I_denoised(row-2, col-2) = (1/(m*n)) * sum(sub(:)); % Compute the arithmetic mean of the neighbourhood and store in output image
     end
 end
+
+% Calculate the mean squared error (MSE) to compare the original grayscale image and the denoised version. 
+[m, n] = size(I_gray);
+sum  = 0;
+for row = 1:m
+    for col = 1:n
+        sum = sum + (double(I_gray(row, col)) - double(I_denoised(row, col)))^2;
+    end
+end
+mse = sum  * ( 1/ (m*n) );
+fprintf('\nThe mean squared error for the denoised image with gaussian noise is %0.4f\n', mse);
 
 % Plot original, noisy and denoised images
 figure(1);
@@ -37,4 +53,4 @@ title('Image with gaussian noise');
 
 subplot(1, 3, 3);
 imshow(uint8(I_denoised));
-title('Image denoised using median filter');
+title('Image denoised using arithmetic mean filter');
